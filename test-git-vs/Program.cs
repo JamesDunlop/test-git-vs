1e87d4e01292f1267a488dc469a362518b7fff8b
James Dunlop <mail.jdunlop@gmail.com>
2015-06-25 21:11:51 +1000

    Writing up State pattern

--- test-git-vs/Program.cs
+++ test-git-vs/Program.cs
@@ -10,9 +10,52 @@ namespace test_git_vs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
-            Console.WriteLine("");
+            var ctx = new Context(new ConcreteStateA());
+            
+            ctx.Request();
+            ctx.Request();
+            ctx.Request();
+            ctx.Request();
+            ctx.Request();
+
             Console.WriteLine("");
         }
     }
+    
+    public class Context 
+    {
+        private State _state;
+        
+        public Context(State state) {
+            _state = state;
+        }    
+        
+        public State State {
+            get { return _state; }
+            set 
+            { 
+                _state = value;
+                Console.WriteLine(_state.GetType().Name); 
+            }
+        }
+        public void Request() {
+            State.Handle(this);
+        }
+    }
+    
+    public abstract class State {
+        public abstract void Handle(Context context);
+    }
+    
+    public class ConcreteStateA : State {
+        public override void Handle(Context context) {
+            context.State = new ConcreteStateB();
+        }
+    }
+   
+    public class ConcreteStateB : State {
+        public override void Handle(Context context) {
+            context.State = new ConcreteStateA();
+        }
+    }    
 }
